4190cca6f697b9d9d8ae5418134b7e3823dd82c0
helenavocado <hwang92@uchicago.edu>
2021-12-04 21:48:54 +0000

    alskdjf

---
+++ app/models/note.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  subject    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+class Note < ApplicationRecord
+
+  belongs_to(:user)
+
+
+  validates(:subject, {:presence => true})
+  validates(:body, {:presence => true})
+
+end
